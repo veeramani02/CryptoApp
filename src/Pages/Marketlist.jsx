ab623a21efe8e2127f6160bbe509e64ba706ceff
veeramani <veeramaniveer64@gmail.com>
2025-08-04 12:30:59 +0530

    correcting path

--- src/Pages/Marketlist.jsx
+++ src/Pages/Marketlist.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Search as SearchIcon, Filter } from "lucide-react";
-import { useCrypto } from "../context/CryptoContext";
+import { useCrypto } from "../Context/CryptoContext";
 import { useNavigate } from "react-router-dom";
 
 const MarketList = () => {
